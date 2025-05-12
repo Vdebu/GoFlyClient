81b9626c87bd6aaa6c770e805bad6c82946e10ab
Vdebu <vdebuzzz@gmail.com>
2025-05-12 19:09:13 +0800

    完成基础的路由鉴权。

--- src/router/index.ts
+++ src/router/index.ts
@@ -4,6 +4,7 @@ import type { Router, RouteRecordRaw } from 'vue-router'
 import { createRouter, createWebHistory } from 'vue-router'
 import { get } from 'lodash'
 import index from '../views/index/index.vue'
+import { LOGIN_PATH } from '../utils/Constants.ts'
 // 拓展封装vue中的原始路由类型 -> 方便后续拓展指定页面下的新页面
 type RouteRecordRawExt = RouteRecordRaw & { children?: RouteRecordRawExt }
 // 收集其他所有模块的路由信息
@@ -44,7 +45,7 @@ export const initRouter: () => Router = () => {
       ],
     },
     {
-      path: '/login',
+      path: LOGIN_PATH,
       name: 'login',
       component: () => import('../views/login/login.vue'),
       meta: {
@@ -73,6 +74,33 @@ export const initRouter: () => Router = () => {
     history: createWebHistory(), // path模式进行路由导航 还有hash可选
     routes: routes, // 表示路由实体中有哪些路由条目
   })
+  // 前置守卫
+  iRouter.beforeEach((to, from, next) => {
+    // to(将要去哪里) from(从哪来) next(处理完成后想去哪里)
+    // 先判断是否登录(看cookie里面有没有token -> ID是否存在)
+    // 通过get方法取保证有默认值兜底防止返回的是null
+    const stLoginUserID = get(app.getAppCtl().getLoginUser(), 'id', '')
+    if (
+      !stLoginUserID &&
+      // 针对每一个记录进行判断
+      to.matched.some(record => false !== get(record, 'meta.requireAuth', true))
+    ) {
+      // 若没登入检查将要访问的路由是否需要权限 -> matched数组中只要有一个路由是要鉴权的就转向登录界面
+      next({
+        path: LOGIN_PATH,
+        query: { redirect: to.fullPath }, // 登录成功后进行跳转
+      })
+      return
+    }
+    // 已登录进入登录界面时直接重定向到主页
+    if (stLoginUserID && to.path === LOGIN_PATH) {
+      next('/')
+      return
+    }
+    // 如果前面两个拦截都失效了就直接放行
+    next()
+  })
+  // 后置守卫
   // 通过守卫方法更改标题信息
   iRouter.afterEach((to, from) => {
     // 尝试获取标题信息(目标，字段值，默认值)
